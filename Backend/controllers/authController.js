324346d1478c9d3646195d272b6dbd85fecb2b1b
Sudhanshu7808 <sudhanshukumar780820@gmail.com>
2026-06-15 20:15:06 +0530

    correct authcon

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -6,6 +6,16 @@ const User = require("../models/user");
 const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key Exists:", !!process.env.CLOUDINARY_API_KEY);
+console.log("API Secret Exists:", !!process.env.CLOUDINARY_API_SECRET);
+
 // Helper function to upload RAM buffers to Cloudinary
 const uploadBufferToCloudinary = (buffer, folderName) => {
   return new Promise((resolve, reject) => {
