49855cfd9966f8ac5cfdebaba65028833aec8b88
Sudhanshu7808 <sudhanshukumar780820@gmail.com>
2026-06-15 19:27:59 +0530

    User change to user

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
-const User = require("../models/User");
+const User = require("../models/user");
 
 // --- NEW IMPORTS FOR VERCEL DEPLOYMENT ---
 const cloudinary = require('cloudinary').v2;
